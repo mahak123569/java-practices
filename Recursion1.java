616ecbe28697bc961a0aa063736aa66a7c930408
mahak <kaushikikes@gmail.com>
2026-03-05 22:57:09 +0530

    changes

--- Recursion1.java
+++ Recursion1.java
@@ -1,13 +1,13 @@
 public class Recursion1 {
     public static void printNumb(int n){
-        if(n == 0){
+        if(n == 6){
             return;
         }
         System.out.println(n);
-        printNumb(n-1);
+        printNumb(n+1);
     }
     public static void main(String[] args) {
-        int n = 5;
+        int n = 1;
         printNumb(n);
     }
 }
