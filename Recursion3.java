ce8e8229833c5b41ad78e31b660dadb06d5d3506
mahak <kaushikikes@gmail.com>
2026-03-10 23:54:21 +0530

    update  file

--- Recursion3.java
+++ Recursion3.java
@@ -1,5 +1,8 @@
 public class Recursion3 {
 public static int calcfactorial( int n){
+    if(n==1 || n == 0){
+    return 1;
+}
 int fact_nm1 = calcfactorial(n-1);
 int fact_n = n* fact_nm1;
 return fact_n;
