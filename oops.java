ceb978bfb769afe573aec592ef0b68110f7ccf72
mahak <kaushikikes@gmail.com>
2026-03-19 22:49:14 +0530

    update

--- oops.java
+++ oops.java
@@ -33,10 +33,12 @@ class Student {
 
     public void printInfo(String name) {
         System.out.println(name);
+
     }
       public void printInfo(int age) {
         System.out.println(age);
     }
+    
   public void printInfo(String name , int age) {
         System.out.println(name + " "+ age);
     }
