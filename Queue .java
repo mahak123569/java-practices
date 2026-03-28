7b44be19727180c7332547ba5d3b2512357fd096
mahak <kaushikikes@gmail.com>
2026-03-28 21:54:52 +0530

    update

--- Queue .java
+++ Queue .java
@@ -1,20 +1,20 @@
 public class Queue {
     static class MyQueue {
-        static int arr[];
-        static int size;
-        static int rear = -1;
+        int arr[];
+        int size;
+        int rear = -1;
 
         MyQueue(int n) {
             arr = new int[n];
             size = n;
         }
 
-        public static boolean isEmpty() {
+        public boolean isEmpty() {
             return rear == -1;
         }
 
         // enqueue
-        public static void add(int data) {
+        public void add(int data) {
             if (rear == size - 1) {
                 System.out.println("full queue");
                 return;
@@ -24,22 +24,25 @@ public class Queue {
         }
 
         // dequeue
-        public static int remove() {
+        public int remove() {
             if (isEmpty()) {
                 System.out.println("empty queue");
                 return -1;
             }
 
             int front = arr[0];
+
+            // shift elements left
             for (int i = 0; i < rear; i++) {
                 arr[i] = arr[i + 1];
             }
+
             rear--;
             return front;
         }
 
         // peek
-        public static int peek() {
+        public int peek() {
             if (isEmpty()) {
                 System.out.println("empty queue");
                 return -1;
@@ -50,9 +53,13 @@ public class Queue {
 
     public static void main(String[] args) {
         MyQueue q = new MyQueue(5);
+
         q.add(1);
         q.add(2);
         q.add(3);
+        q.add(4);
+        q.add(5);
+        q.add(6); // will show "full queue"
 
         while (!q.isEmpty()) {
             System.out.println(q.peek());
