26a7719470af14ee592c0c4f6fcdec99641e4683
mahak <kaushikikes@gmail.com>
2026-03-30 22:47:56 +0530

    add

---
+++ Adjacency.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+public class Adjacency {
+
+    static class Edge {
+        int src;
+        int dest;
+
+        public Edge(int s, int d) {
+            this.src = s;
+            this.dest = d;
+        }
+    }
+
+    public static void createGraph(ArrayList<Edge> graph[]) {
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        graph[0].add(new Edge(0, 2));
+
+        graph[1].add(new Edge(1, 2));
+        graph[1].add(new Edge(1, 3));
+
+        graph[2].add(new Edge(2, 0));
+        graph[2].add(new Edge(2, 1));
+        graph[2].add(new Edge(2, 3));
+
+        graph[3].add(new Edge(3, 1)); // fixed (21 → 1)
+        graph[3].add(new Edge(3, 2));
+    }
+
+    public static void main(String[] args) {
+        int V = 4;
+        ArrayList<Edge>[] graph = new ArrayList[V];
+
+        createGraph(graph);
+
+        // Print graph
+        for (int i = 0; i < V; i++) {
+            System.out.print(i + " -> ");
+            for (int j = 0; j < graph[i].size(); j++) {
+                Edge e = graph[i].get(j);
+                System.out.print(e.dest + " ");
+            }
+            System.out.println();
+        }
+    }
+}
